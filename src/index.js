1a60f5ffbf9a063c0f79bf57cfd9df7e6ae3eddc
Jose Rodriguez <jrodriguez2076@gmail.com>
2019-12-10 09:01:46 -0300

    Working express server and react app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, (req, res) => {
+app.listen(3000, () => {
     console.log('Now listening on port 3000');
-    console.log("heres is your request: " + req);
 })
